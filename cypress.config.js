afb91dd9773c4fd27d35a0529ffba32d07e6058d
Bond-z <nathakrit.p@gmail.com>
2024-09-16 12:08:06 +0700

    change cicd

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,6 @@
 const cypress = require("cypress");
 const { defineConfig } = require("cypress");
-// import registerCypressGrep from '@cypress/grep/src/support';       //Use import in .js  when the project is configured as an ES module ("type": "module" in package.json).
-// const registerCypressGrep = require('@cypress/grep/src/support');    //Use require in .cjs files.
-// const registerCypressGrep = require('cypress-grep/src/support');
+
 const cypressGrep = require('cypress-grep/src/plugin');
 
 // @type {Cypress.PluginConfig}
